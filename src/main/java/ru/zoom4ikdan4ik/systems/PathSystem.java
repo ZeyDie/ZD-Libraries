2e21436e888180a78b5f5c340f3e204e09752a81
Zoom4ikDan4ik <zoom4ikdan4ik@gmail.com>
2020-11-07 02:52:49 +0300

    Some changes #2

--- src/main/java/ru/zoom4ikdan4ik/systems/PathSystem.java
+++ src/main/java/ru/zoom4ikdan4ik/systems/PathSystem.java
@@ -1,16 +1,23 @@
 package ru.zoom4ikdan4ik.systems;
 
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.CodeSource;
+import java.security.ProtectionDomain;
 
 public final class PathSystem {
     public static Path getPathLibrary() {
-        final URL location = PathSystem.class.getProtectionDomain().getCodeSource().getLocation();
+        final ProtectionDomain protectionDomain = PathSystem.class.getProtectionDomain();
+        final CodeSource codeSource = protectionDomain.getCodeSource();
+        final URL location = codeSource.getLocation();
 
         try {
-            return Paths.get(location.toURI());
+            final URI uri = location.toURI();
+
+            return Paths.get(uri);
         } catch (URISyntaxException error) {
             error.printStackTrace();
 
